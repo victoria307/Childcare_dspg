45daf6c4e1413d33be0c37d41d7df8ef7b4b323e
vtjpyoo <jpyoo@vt.edu>
2025-06-23 09:55:26 -0400

    Updating File Structure

---
+++ Program/SIPP/SIPP Data Cleaning.R
@@ -0,0 +1,268 @@
+
+# File Overview -----------------------------------------------------------
+
+#This code will 
+#1. Upload the file to rstudio
+#2. Extract useful variables
+#3. Clean the variables to be ready to plug into a regression
+#4. Rename Columns for clarity
+#5. Download completed File
+
+
+
+# File Setup --------------------------------------------------------------
+
+#Clearing the workspace
+cat("\014")
+rm(list = ls())
+
+#setting the WD where Data is housed
+setwd("C:/Users/joshu/Desktop/DSPG/Code/SIPP")
+
+
+#Packages needed (install.packages if these lines do not work)
+library("data.table")
+library("bit64")
+library("dplyr")
+library("tidyr")
+library("forcats")
+library(stringr)
+
+
+
+# Data Extraction ---------------------------------------------------------
+
+#Pulling the Data
+extracted_data <- unzip("pu2023_csv.zip", "pu2023.csv")
+
+
+#Getting all useful variables
+pu <- fread(extracted_data, sep = "|", select = c(
+  #Identifier
+  'EDOB_BMONTH', 'EEDUC', 'EMS', 'ERACE', 'ESEX', 
+  'RCHTYP1', 'RCHTYP2', 'RCHTYP3','RCHTYP4', 'RCHTYP5', 'RCHTYP6', 'RCHTYP7', 'RCHTYP8',
+  'RCHTYP9', 'RCHTYP10',
+  'RFAMKIND', 'TAGE', 'TDOB_BYEAR','TIMSTAT', 'TRACE', 'TYRENTRY', 'ENATCIT',
+  'TAGE_FB', 'TEHC_METRO', 'TEHC_REGION',
+  #Childcare
+  'ECHLD_ANY', 'EDAYCARE', 'EDAYHS', 'EFAM', 'EGRAN','EHEADST','EKIDSELF', 'ELIST',
+  #'ECREPAYANYON', DNE, only in 2020
+  'ENREL', 'ENUR','ENURHS', 'EOTHR','EPAR', 'EPAY', 'EPAYHELP','EPROG','ESELF',
+  'ESIB15','ETIMELOST','ETIMELOST_TP','EWHOPAID1','EWHOPAID2','EWHOPAID3','EWHOPAID4',
+  'EWHOPAID5','EWORKMORE','TPAYWK','RDAYCARE','RDAYHS','RFAM','RGRAN','RHEADST',
+  'RKIDSELF','RNREL','RNUR','RNURHS','ROTHR','RPAR','RPROG','RSELF','RSIB15',
+  #Controls
+  'RGED','EJB1_DYSWKD','EJB2_DYSWKD','SPANEL',
+  #Labor Supply
+  'EJB1_AWOP1','EJB1_PTRESN1','EJB1_WSMNR',
+  'TWKHRS1','TWKHRS2', 'TWKHRS3', 'TWKHRS4', 'TWKHRS5',
+  'TMWKHRS',"EJB1_PVWKTR9", "EJB1_SCRNR", "THTOTINC",
+  #Child Variables
+  'TCBYR_1', 'TCBYR_2', 'TCBYR_3', 'TCBYR_4', 'TCBYR_5', 'TCBYR_6',
+  # Any number after is suppressed
+  "SHHADID", "MONTHCODE", "PNUM", "SSUID"
+  
+))
+
+#Choosing Variables needed for Model
+data <- pu %>% 
+  select("SSUID","SHHADID","PNUM","MONTHCODE","SPANEL","EEDUC","TAGE","ESEX", "ERACE", "EMS", "RFAMKIND", "RFAMKIND", "TEHC_METRO", "TEHC_REGION","EOTHR", "THTOTINC",
+         "EPAR", "EPAYHELP","ETIMELOST", "ETIMELOST_TP", "EWHOPAID1", "ELIST", "TWKHRS1", "TWKHRS2", "TWKHRS3", "TWKHRS4",
+         "TWKHRS5", "TMWKHRS", "EJB1_PVWKTR9", 'TCBYR_1', 'TCBYR_2', 'TCBYR_3', 'TCBYR_4', 'TCBYR_5', 'TCBYR_6', "EJB1_SCRNR" 
+         )
+
+
+
+# Data Cleaning -----------------------------------------------------------
+
+
+
+## Filtering --------------------------------------------------------------
+#Filtering for working age adults 
+data <- data %>% 
+  filter(TAGE >= 18) %>% 
+  filter(TAGE <= 67) 
+
+
+
+## Outcome Variables ----
+
+#Harmonizing Hours lost into standard unit (hours)
+#ETIMELOST_TP
+#1: Hours 
+#2: Days 
+#3: Weeks
+data <- data %>%
+  mutate(TimeLost = case_when(
+    ETIMELOST_TP == 1 ~ ETIMELOST * 1,
+    ETIMELOST_TP == 2 ~ ETIMELOST * 24,
+    ETIMELOST_TP == 3 ~ ETIMELOST * 24 * 7,
+    TRUE ~ 0
+  ))
+summary(data$TimeLost)
+sum(data$TimeLost>0)
+#Issue: 1740 out of 28000+ people lost hours
+
+#Employment Indicator
+data <- data %>% 
+  mutate(EMP = if_else(EJB1_SCRNR == 1,1,0))
+
+#Hours Worked
+data <- data %>% 
+  mutate(HRSWorked = TMWKHRS) %>% 
+  mutate(HRSWorked = replace_na(HRSWorked,0)) 
+
+
+## Child Variables -------------------------------------------
+
+#Creating Indicator Variables for kids under the age of 13
+data <- data %>% 
+  mutate(
+    Kid1 = case_when(SPANEL - TCBYR_1 <= 13 & SPANEL - TCBYR_1 >= 0 ~ 1, TRUE ~ 0),
+    Kid2 = case_when(SPANEL - TCBYR_2 <= 13 & SPANEL - TCBYR_2 >= 0 ~ 1, TRUE ~ 0),
+    Kid3 = case_when(SPANEL - TCBYR_3 <= 13 & SPANEL - TCBYR_3 >= 0 ~ 1, TRUE ~ 0),
+    Kid4 = case_when(SPANEL - TCBYR_4 <= 13 & SPANEL - TCBYR_4 >= 0 ~ 1, TRUE ~ 0),
+    Kid5 = case_when(SPANEL - TCBYR_5 <= 13 & SPANEL - TCBYR_5 >= 0 ~ 1, TRUE ~ 0),
+    Kid6 = case_when(SPANEL - TCBYR_6 <= 13 & SPANEL - TCBYR_6 >= 0 ~ 1, TRUE ~ 0)
+  )
+
+#Creating Age of Child Buckets
+# 1 = infant (0 - 1)
+# 2 = toddler (2 -3)
+# 3 = Preschool (4-5)
+# 4 = School age (6 - 13)
+# 5 = Too Old (14+)
+# -1 = Child does not exist
+data <- data %>%
+  mutate(age1 = if_else(is.na(TCBYR_1), -1, SPANEL - TCBYR_1),
+         age_group1 = cut(
+           age1,
+           breaks = c(-Inf, -1, 1, 3, 5, 13, Inf),
+           labels = c("No Child", "Infant", "Toddler", "Preschool", "School Age", "Past Subsidy"),
+           right = TRUE
+         ),
+         age2 = if_else(is.na(TCBYR_2), -1, SPANEL - TCBYR_2),
+         age_group2 = cut(
+           age2,
+           breaks = c(-Inf, -1, 1, 3, 5, 13, Inf),
+           labels = c("No Child", "Infant", "Toddler", "Preschool", "School Age", "Past Subsidy"),
+           right = TRUE
+         ),
+         age3 = if_else(is.na(TCBYR_3), -1, SPANEL - TCBYR_3),
+         age_group3 = cut(
+           age3,
+           breaks = c(-Inf, -1, 1, 3, 5, 13, Inf),
+           labels = c("No Child", "Infant", "Toddler", "Preschool", "School Age", "Past Subsidy"),
+           right = TRUE),
+         age4 = if_else(is.na(TCBYR_4), -1, SPANEL - TCBYR_4),
+         age_group4 = cut(
+           age4,
+           breaks = c(-Inf, -1, 1, 3, 5, 13, Inf),
+           labels = c("No Child", "Infant", "Toddler", "Preschool", "School Age", "Past Subsidy"),
+           right = TRUE),
+         age5 = if_else(is.na(TCBYR_5), -1, SPANEL - TCBYR_5),
+         age_group5 = cut(
+           age5,
+           breaks = c(-Inf, -1, 1, 3, 5, 13, Inf),
+           labels = c("No Child", "Infant", "Toddler", "Preschool", "School Age", "Past Subsidy"),
+           right = TRUE),
+         age6 = if_else(is.na(TCBYR_6), -1, SPANEL - TCBYR_6),
+         age_group6 = cut(
+           age6,
+           breaks = c(-Inf, -1, 1, 3, 5, 13, Inf),
+           labels = c("No Child", "Infant", "Toddler", "Preschool", "School Age", "Past Subsidy"),
+           right = TRUE)
+  )
+
+
+#Waiting List (ELISt)
+#23k NA's, 21k when filtering for Kid1, so not useful
+
+summary(data$ELIST)
+summary(data$ELIST[data$Kid1 == 1])
+
+
+## Primary Individual Variables ------
+
+#Education Buckets
+#Less than HS, HS, undergrad, More than College
+data <- data %>%
+  mutate(Education = cut(
+    EEDUC,
+    breaks = c(-Inf, 38, 42, 43, Inf),
+    labels = c("No High School Diploma", "High School or Some College",
+               "Bachelor’s Degree", "Graduate or Professional Degree"),
+    right = TRUE
+  ))
+
+
+#Age of Primary Parent
+data <- data %>% 
+  mutate(ParentAge = TAGE)
+
+#Race of Parent
+data$ERACE <- as.factor(data$ERACE)
+data <- data %>% 
+  mutate(Race = factor(ERACE,
+                       levels = c(1,2,3,4),
+                       labels = c("White", "Black", "Asian", "Residual")))
+
+#Income
+summary(data$THTOTINC)
+sum(is.na(data$THTOTINC))
+data <- data %>% 
+  mutate(Income = THTOTINC)
+
+#Household ID
+data <- data %>% 
+  mutate(HouseholdID = SHHADID,
+         PersonalNumber = PNUM,
+         PID = paste(SHHADID, PNUM, sep = ""),
+         Month = MONTHCODE,
+         Year = SPANEL,
+         TimeID = paste(Month, Year, sep = "")) 
+
+
+
+## Control Factors----
+#Make Metro into a factor
+#TEHC_METRO
+#1: Metropolitan Area
+#2: Nonmetropolitan Area  
+data <- data %>% 
+  filter(TEHC_METRO != 3) %>% 
+  mutate(NonMetro = if_else(TEHC_METRO == 2,1,0))
+
+
+#Making sex into a factor
+data <- data %>% 
+  mutate(Female = if_else(data$ESEX == 2,1,0))
+
+
+#Create EMS into a usable Factor
+data <- data %>% 
+  mutate(SingleFamily = if_else(EMS != 1,1,0))
+
+
+#Making EPAYHELP into a factor
+data <- data %>% 
+  mutate(WelfareorSS = if_else(EWHOPAID1 == 1,1,0)) %>% 
+  mutate(WelfareorSS = replace_na(WelfareorSS,0))
+
+#Creating Work from home Indicator
+data <- data %>% 
+  mutate(WorkFromHome = if_else(EJB1_PVWKTR9 == 1,1,0)) %>% 
+  mutate(WorkFromHome = replace_na(WorkFromHome,0))
+summary(data$WorkFromHome)
+
+
+data2 <- data %>% 
+  select(36:ncol(data)) %>% 
+  filter(Kid1 == 1)
+
+
+write.csv(data2,"cleaneddata.csv")
+
+
+
+
---
+++ Program/SIPP/SIPP Model Creation2.R
@@ -0,0 +1,220 @@
+
+
+# File Setup --------------------------------------------------------------
+
+
+cat("\014")
+rm(list = ls())
+
+setwd("C:/Users/joshu/Desktop/DSPG/Code/SIPP")
+
+
+
+library("data.table")
+library("bit64")
+library("dplyr")
+library("stargazer")
+library(forcats)
+library(broom)
+library(glmnet)
+library(fixest)
+library(modelsummary)
+
+
+
+# Data Collection + Cleaning ----------------------------------------------
+
+
+data <- read.csv("cleaneddata.csv")
+
+
+#Making Factors
+factor_cols <- c("age_group1",
+                 "age_group2",
+                 "age_group3",
+                 "age_group4",
+                 "age_group5",
+                 "age_group6")
+factor_cols2 <- c("age_group2",
+                  "age_group3",
+                  "age_group4",
+                  "age_group5",
+                  "age_group6")
+data[factor_cols] <- lapply(data[factor_cols], factor)
+data <- data %>%
+  mutate(across(all_of(factor_cols2), ~ fct_relevel(., "No Child")))
+
+data$Race <- as.factor(data$Race)
+data$Race <- factor(data$Race,
+                    levels = c("Residual", "White", "Black", "Asian"))
+
+
+
+# Model Creation ----------------------------------------------------------
+
+## Basic Models -------
+
+TimeLost1 <- lm(
+  TimeLost ~ NonMetro + Female + SingleFamily + WelfareorSS +
+    Education + ParentAge + WorkFromHome + Race + Income +
+    Kid2 + Kid3 + Kid4 + Kid5 + Kid6,
+  data
+)
+summary(TimeLost1)
+modelsummary(TimeLost1, 
+             stars = TRUE,
+             statistic = "p.value",
+             output = "latex")
+
+
+# Logistic, removed work from home
+EMP1 <- glm(
+  EMP ~ NonMetro + Female + SingleFamily + WelfareorSS +
+    Education + ParentAge + Race + Income +
+    Kid2 + Kid3 + Kid4 + Kid5 + Kid6,
+  data,
+  family = binomial
+)
+summary(EMP1)
+modelsummary(EMP1, 
+             stars = TRUE,
+             statistic = "p.value",
+             output = "latex")
+
+
+HRSWorked1 <- lm(
+  HRSWorked ~ NonMetro + Female + SingleFamily + WelfareorSS +
+    Education + ParentAge + WorkFromHome + Race + Income +
+    Kid2 + Kid3 + Kid4 + Kid5 + Kid6,
+  data
+)
+summary(HRSWorked1)
+modelsummary(HRSWorked1, 
+             stars = TRUE,
+             statistic = "p.value",
+             output = "latex")
+
+## Fixed Effects Model ----
+
+TimeLostFE <- feols(
+  TimeLost ~ NonMetro + Female + SingleFamily + WelfareorSS +
+    Education + ParentAge + WorkFromHome + Race + Income +
+    Kid2 + Kid3 + Kid4 + Kid5 + Kid6 |HouseholdID +PersonalNumber + TimeID,
+  data
+)
+summary(TimeLostFE)
+modelsummary(TimeLostFE, 
+             stars = TRUE,
+             statistic = "p.value",
+             output = "latex")
+
+EMPFE <- feglm(
+  EMP ~ NonMetro + Female + SingleFamily + WelfareorSS +
+    Education + ParentAge + WorkFromHome + Race + Income +
+    Kid2 + Kid3 + Kid4 + Kid5 + Kid6 | HouseholdID + PersonalNumber + TimeID,
+  data,
+  family = binomial()
+)
+summary(EMPFE)
+modelsummary(EMPFE, 
+             stars = TRUE,
+             statistic = "p.value",
+             output = "latex")
+
+HRSWorkedFE <- feols(
+  HRSWorked ~ NonMetro + Female + SingleFamily + WelfareorSS +
+    Education + ParentAge + WorkFromHome + Race + Income +
+    Kid2 + Kid3 + Kid4 + Kid5 + Kid6 | HouseholdID + PersonalNumber + TimeID,
+  data
+)
+summary(HRSWorkedFE)
+modelsummary(HRSWorkedFE, 
+             stars = TRUE,
+             statistic = "p.value",
+             output = "latex")
+
+## Interaction Models ----
+
+TimeLost2 <- lm(
+  TimeLost ~ NonMetro + Female + SingleFamily + WelfareorSS +
+    Education + ParentAge + WorkFromHome + Race + Income +
+    Kid2 + Kid3 + Kid4 + Kid5 + Kid6 +
+    age_group1 + Kid2:age_group2 + Kid3:age_group3 + Kid4:age_group4 + Kid5:age_group5 +
+    Kid6:age_group6,
+  data
+)
+summary(TimeLost2)
+
+
+EMP2 <- lm(
+  EMP ~ NonMetro + Female + SingleFamily + WelfareorSS +
+    Education + ParentAge + WorkFromHome + Race + Income +
+    Kid2 + Kid3 + Kid4 + Kid5 + Kid6 +
+    age_group1 + Kid2:age_group2 + Kid3:age_group3 + Kid4:age_group4 + Kid5:age_group5 +
+    Kid6:age_group6,
+  data
+)
+summary(EMP2)
+
+
+HRSWorked2 <- lm(
+  HRSWorked ~ NonMetro + Female + SingleFamily + WelfareorSS +
+    Education + ParentAge + WorkFromHome + Race + Income +
+    Kid2 + Kid3 + Kid4 + Kid5 + Kid6 +
+    age_group1 + Kid2:age_group2 + Kid3:age_group3 + Kid4:age_group4 + Kid5:age_group5 +
+    Kid6:age_group6,
+  data
+)
+summary(HRSWorked2)
+
+
+## Hurdle Model ----
+### Data Adjustments
+data2 <- data %>% 
+  mutate(TimeLostFactor = if_else(TimeLost>0,1,0))
+
+### Hurdle Part 1 ----
+Hurdle1<- glm(
+  TimeLostFactor ~ NonMetro + Female + SingleFamily + WelfareorSS +
+    Education + ParentAge + WorkFromHome + Race + Income +
+    Kid2 + Kid3 + Kid4 + Kid5 + Kid6,
+  data2,
+  family = binomial
+)
+summary(Hurdle1)
+
+
+### Hurdle Part 2 ----
+data3 <- data2 %>% 
+  filter(TimeLostFactor == 1)
+
+Hurdle2<- lm(
+  TimeLost ~ NonMetro + Female + SingleFamily + WelfareorSS +
+    Education + ParentAge + WorkFromHome + Race + Income +
+    Kid2 + Kid3 + Kid4 + Kid5 + Kid6,
+  data3
+)
+
+summary(Hurdle2)
+
+stargazer(Hurdle1,Hurdle2)
+
+# LASSO ----
+
+
+LASSOData <- data %>% 
+  select(c(-1,-2,-3,-4, -11,-13, -15, -17, -19, -21))
+
+EMP <- data$EMP
+TimeLost <- data$TimeLost
+HoursWorked <- data$HRSWorked
+LASSOData <- data.matrix(LASSOData)
+
+
+## Employment ----
+
+EMPModel <- cv.glmnet(LASSOData, EMP, alpha = 1)
+
+
+
+
